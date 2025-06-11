c29b10ebbb82e9eb32eeeebe9cd6b3a39b9d9d99
SG <sg@example.com>
2025-06-11 08:08:14 +0545

    casting and type

--- first-program.py
+++ first-program.py
@@ -1,3 +1,7 @@
 name = input("enter your name: ")
+age = int(input("enter your age: "))
 print("hello "+name) #concatenation
 print(f"hello, {name}")  #string formatting
+print(type(age)) #type of variable
+print("age is: ",int(age))
+ 
