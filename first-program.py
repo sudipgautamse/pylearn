e5c2235cf810eebc12b7e5ddbeb16cb35c4fd0bb
SG <sg@example.com>
2025-06-11 08:55:18 +0545

    for loop

--- first-program.py
+++ first-program.py
@@ -1,9 +1,9 @@
 name = input("enter your name: ")
 print("hello "+name) #concatenation
 print(f"hello, {name}")  #string formatting
-while True:
-    try:
-        age = int(input("enter your age: "))
+#while True:
+#    try:
+#        age = int(input("enter your age: "))
         #operators
         # == equals to
         # != not equals to
@@ -15,19 +15,33 @@ while True:
         # and, or, not
         # if, elif, else
         
+#        if age < 16:
+#            print("you are a minor")
+#        elif age > 60:
+#            print("you are elderly")
+#        else:
+#            print("you are eligible to work")
+#            print(type(age)) #type of variable
+#            print("age is: ",int(age))
+#        print(f"your age is: {age}")
+#    except:
+#        print("please give numbers")
+#    run = input("do you want to continue??(y/n)\n")
+#    if run == "n":
+#        break
+for i in range(2,6,2):
+    try:
+        age = int(input("enter your age: "))
         if age < 16:
             print("you are a minor")
         elif age > 60:
             print("you are elderly")
         else:
             print("you are eligible to work")
-    #print(type(age)) #type of variable
-    #print("age is: ",int(age))
+            print(type(age)) #type of variable
+            print("age is: ",int(age))
         print(f"your age is: {age}")
     except:
         print("please give numbers")
-    run = input("do you want to continue??(y/n)\n")
-    if run == "n":
-        break
 
  
