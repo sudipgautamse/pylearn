7720c845f3e86af9490b2d007e671736c97c5096
SG <sg@example.com>
2025-06-11 08:17:36 +0545

    exception handling

--- first-program.py
+++ first-program.py
@@ -1,7 +1,12 @@
-name = input("enter your name: ")
-age = int(input("enter your age: "))
-print("hello "+name) #concatenation
-print(f"hello, {name}")  #string formatting
-print(type(age)) #type of variable
-print("age is: ",int(age))
+#name = input("enter your name: ")
+try:
+    age = int(input("enter your age: "))
+#print("hello "+name) #concatenation
+#print(f"hello, {name}")  #string formatting
+#print(type(age)) #type of variable
+#print("age is: ",int(age))
+    print(f"your age is: {age}")
+except:
+    print("please give numbers")
+
  
