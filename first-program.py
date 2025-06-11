c6e417c3ce3a6e08168bd9e932c49677715286d6
SG <sg@example.com>
2025-06-11 08:41:14 +0545

    if else looping

--- first-program.py
+++ first-program.py
@@ -1,29 +1,33 @@
 name = input("enter your name: ")
 print("hello "+name) #concatenation
 print(f"hello, {name}")  #string formatting
-try:
-    age = int(input("enter your age: "))
-    #operators
-    # == equals to
-    # != not equals to
-    # > greater than
-    # <less than
-    # >= greater than or equals to
-    # <= less than or equals to
-    # logical operators
-    # and, or, not
-    # if, elif, else
-    
-    if age < 16:
-        print("you are a minor")
-    elif age > 60:
-        print("you are elderly")
-    else:
-        print("you are eligible to work")
-#print(type(age)) #type of variable
-#print("age is: ",int(age))
-    print(f"your age is: {age}")
-except:
-    print("please give numbers")
+while True:
+    try:
+        age = int(input("enter your age: "))
+        #operators
+        # == equals to
+        # != not equals to
+        # > greater than
+        # <less than
+        # >= greater than or equals to
+        # <= less than or equals to
+        # logical operators
+        # and, or, not
+        # if, elif, else
+        
+        if age < 16:
+            print("you are a minor")
+        elif age > 60:
+            print("you are elderly")
+        else:
+            print("you are eligible to work")
+    #print(type(age)) #type of variable
+    #print("age is: ",int(age))
+        print(f"your age is: {age}")
+    except:
+        print("please give numbers")
+    run = input("do you want to continue??(y/n)\n")
+    if run == "n":
+        break
 
  
